6a9e55c2322339efda2620da58dc6033410907b4
José Pedro Dias <jose.pedro.dias@gmail.com>
2017-12-25 03:20:13 +0000

    basic register and logout. ongoing LOB

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import "./App.css";
 
 import * as tsmc from "./toy-stock-market-client";
 
+function LogoutForm(props) {
+  return (
+    <div>
+      {props.username}
+      <button onClick={props.logout}>logout</button>
+    </div>
+  );
+}
+
 function LoginForm(props) {
   let uEl, pEl;
   return (
@@ -17,35 +26,111 @@ function LoginForm(props) {
       <br />
       <button
         onClick={() => {
-          props.cb(uEl.value, pEl.value);
+          props.login(uEl.value, pEl.value);
         }}
       >
         login
       </button>
+      <button
+        onClick={() => {
+          props.register(uEl.value, pEl.value, 1000);
+        }}
+      >
+        register
+      </button>
     </div>
+  );
+}
+
+function LOB(props) {
+  if (!props.data) return "";
+  const bids = props.data.bids;
+  const asks = props.data.asks;
+  return (
+    <table className="lob">
+      <tbody>
+        <tr>
+          <td>bid</td>
+          <td>ask</td>
+        </tr>
+        <tr>
+          <td>
+            {bids.map(b => (
+              <div>
+                <span>{b.quantity}</span>|
+                <span>{b.price}</span>
+              </div>
+            ))}
+          </td>
+          <td>
+            {asks.map(b => (
+              <div>
+                <span>{b.quantity}</span>|
+                <span>{b.price}</span>
+              </div>
+            ))}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
 
 class App extends Component {
   state: {
     username: ?string,
-    stockNames: Array<string>
+    stockNames: Array<string>,
+    stockLobs: { [string]: Object },
+    money: number,
+    owns: { [string]: number }
   };
 
   state = {
     username: undefined,
-    stockNames: []
+    stockNames: [],
+    stockLobs: {},
+    money: 0,
+    owns: {}
+  };
+
+  updateOwns = () => {
+    tsmc.trader().then(o => {
+      this.setState({ money: o.money });
+    });
   };
 
   login = (user: string, pass: string) => {
     tsmc.login(user, pass).then(() => {
       this.setState({ username: tsmc.isLoggedIn() });
+      this.updateOwns();
+    });
+  };
+
+  register = (user: string, pass: string, money: number) => {
+    tsmc.register(user, pass, money).then(() => {
+      this.setState({ username: tsmc.isLoggedIn() });
+      this.updateOwns();
+    });
+  };
+
+  logout = () => {
+    tsmc.logout().then(() => {
+      this.setState({ username: undefined, owns: {}, money: 0 });
     });
   };
 
   componentWillMount() {
     tsmc.stocks().then(names => {
       this.setState({ stockNames: names });
+      names.forEach(name => {
+        tsmc.stockLOB(name).then(lob => {
+          this.setState((state, props) => {
+            const lobs = this.state.stockLobs;
+            lobs[name] = lob;
+            return { stockLobs: lobs };
+          });
+        });
+      });
     });
   }
 
@@ -53,13 +138,30 @@ class App extends Component {
     return (
       <div>
         {this.state.username ? (
-          this.state.username
+          <LogoutForm username={this.state.username} logout={this.logout} />
         ) : (
-          <LoginForm cb={this.login} />
+          <LoginForm login={this.login} register={this.register} />
         )}
 
+        <h2>owns:</h2>
+        <ul>
+          {Object.keys(this.state.owns).map(own => {
+            return (
+              <li>
+                {own}: {this.state.owns[own]}
+              </li>
+            );
+          })}
+        </ul>
+
         <h2>stocks:</h2>
-        <ul>{this.state.stockNames.map(n => <li key={n}>{n}</li>)}</ul>
+        <ul>
+          {this.state.stockNames.map(n => (
+            <li key={n}>
+              {n}: <LOB data={this.state.stockLobs[n]} />
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
